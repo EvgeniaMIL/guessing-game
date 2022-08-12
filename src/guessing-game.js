a6f8e46e5d0a6748efed1c08bee7b8c09d5a489f
EvgeniaMIL <e.vaylet@mail.ru>
2022-08-12 14:08:43 +0300

    feat:add decision task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        return this.num = Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.num
     }
 
     greater() {
-
+        this.min = this.num
     }
 }
 
